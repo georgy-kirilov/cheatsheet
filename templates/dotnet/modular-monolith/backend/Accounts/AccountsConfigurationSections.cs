3360e072c4c43f801d6132b62616f6239f801ed9
georgy-kirilov <58916152+georgy-kirilov@users.noreply.github.com>
2023-11-19 13:34:49 +0200

    Update nuget packages versions.

--- templates/dotnet/modular-monolith/backend/Accounts/AccountsConfigurationSections.cs
+++ templates/dotnet/modular-monolith/backend/Accounts/AccountsConfigurationSections.cs
@@ -2,7 +2,9 @@ namespace Accounts;
 
 public static class AccountsConfigurationSections
 {
-    public const string Password = "Password";
-    public const string SignIn = "SignIn";
     public const string User = "User";
+
+    public const string Password = "Password";
+    
+    public const string SignIn = "SignIn";
 }
